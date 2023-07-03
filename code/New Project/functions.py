d1726f00d1ddf7ea355fe67d01a891df63450e55
Rafe Sharif <rafe.sh2001@gmail.com>
2023-07-03 12:18:47 +0330

    new project files

---
+++ code/New Project/functions.py
@@ -0,0 +1,124 @@
+def preprocess_cluster(data, g_mean_th=18):
+    """
+    ------
+    parallax > 0 
+
+    phot_g_mean_mag < g_mean_th
+    ------
+    """
+    data = data[data['parallax'] > 0]
+    data = data[data['phot_g_mean_mag'] < g_mean_th]
+    data['Gmg'] = data['phot_g_mean_mag'] + (5 * np.log10(data['parallax']) - 10)
+    data['L'] = 10**(0.4*(4.83 - data['Gmg']))
+    print(len(data))
+    return data
+
+
+def cmd_plot(data, x_axis, y_axis, alpha=0.8, s=5):
+    """
+    -------
+    plot isochron
+    -------
+    """
+
+    with plt.style.context(['ieee']):
+        fig = plt.figure(figsize=(6,6), dpi=100)
+        sns.scatterplot(data=data, y=y_axis, x=x_axis, alpha=alpha, s=s)
+        plt.gca().invert_yaxis();
+        
+        
+def joint_plot(data):
+
+    plt.figure(dpi=90)
+    sns.jointplot(
+        data=data,
+        x="pmra", y="pmdec",
+        kind="kde"
+        );
+    
+    
+def fit_curve(data, column, bins = 100):
+
+    plt.figure(figsize=(12,3), dpi=120)
+    counts, bins, patches = plt.hist(data[column], bins = bins)
+
+    # Define the Gaussian function
+    def gaussian(x, amp, mu, sigma):
+        return amp * np.exp(-(x - mu)**2 / (2 * sigma**2))
+    
+    # data
+    x_data = bins[:-1]
+    y_data = counts
+    
+    # Fit the Gaussian function to the data
+    popt, pcov = curve_fit(gaussian, x_data, y_data)
+    
+    plt.figure(figsize=(12,3), dpi=120)
+    # Plot the original data and the fitted curve
+    sns.scatterplot(x_data, y_data, label=column)
+    plt.plot(x_data, gaussian(x_data, *popt), color='red', label='Fit')
+    plt.legend()
+    plt.show()
+    
+    return popt
+
+
+def guassian_filter(data, column, mu, std):    
+    up = round(mu + 3 * std, 2)
+    low = round(mu - 3 * std, 2)
+    print('upper bound:', up) 
+    print('lower bound:', low)
+    
+    if up > low:
+        df = data[(data['pmra']< up) & (data['pmra'] > low)]
+    else:
+        df = data[(data['pmra']< low) & (data['pmra'] > up)]
+    
+    print('cluster length:', len(df))
+    return df
+
+
+def luminosity_density(cluster_3d, clusterdf):
+    """
+    --------
+    cluster_3d --> measuring distance in that dataframe
+    
+    clusterdf --> main dataframe
+    -------
+    """
+    # create a NearestNeighbors object and fit the dataset
+    nbrs = NearestNeighbors(n_neighbors=6, metric='minkowski').fit(cluster_3d)
+
+    # find the 5 nearest neighbors for each data point including itself
+    distances, indices = nbrs.kneighbors(cluster_3d)
+
+    # find maximum distance among 5 neighbors
+    max_distances = np.amax(distances, axis=1)
+
+    # sphere of that max distance
+    spheres = (4/3) * np.pi * (max_distances ** 3)
+
+    # sum of luminosities of each 6 nn
+    lum_sum = []
+    for i in range(len(clusterdf)):
+        lum_sum.append(np.sum(clusterdf.iloc[indices[i]]['L']))
+
+    # luminosity density
+    lum_dens = lum_sum / spheres
+
+    return lum_dens
+
+
+
+def lum_plot(data):
+    """
+    --------
+    plot for luminosity density profile
+    --------
+    """    
+    
+    with plt.style.context(['ieee']):
+        plt.figure(figsize=(12,6), dpi=200)
+        plt.plot(range(len(data)), np.sort(data))
+        plt.ylabel('ΔL/ΔV')
+        plt.title('luminosity density profile');
